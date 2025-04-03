ed7dce4a030066d9c1ac99f2b11b0120089199e9
Friedrich <friedrich.eibl@hu-berlin.de>
2025-04-03 17:03:34 -0400

    add bird recordings for testing

--- tests_db.py
+++ tests_db.py
@@ -4,6 +4,13 @@
 #         "Bach": "songs/1721 Bach , Minuet and Badinerie (from Orchestral Suite No. 2 inB Minor).mp3",
 #         # Add more 'Song Name': 'path/to/file.mp3' entries
 #     }
+# known_songs = {
+#         "NC1": "northern_cardinal_clips/XC899256 - Northern Cardinal - Cardinalis cardinalis.wav",
+#         "NC2": "northern_cardinal_clips/XC899257 - Northern Cardinal - Cardinalis cardinalis.wav",
+#         "NC3": "northern_cardinal_clips/XC920144 - Northern Cardinal - Cardinalis cardinalis.mp3",
+#         "NC4": "northern_cardinal_clips/XC920145 - Northern Cardinal - Cardinalis cardinalis.mp3",
+#         "NC5": "northern_cardinal_clips/XC930723 - Northern Cardinal - Cardinalis cardinalis.wav",
+#     }
 # conn = sqlite3.connect('fingerprint_database.db') # Connect once
 # for song_name, path in known_songs.items():
 #     song_id = add_song_to_db(conn, song_name, path)
@@ -13,12 +20,15 @@
 #         song_hashes = generate_fingerprints(peaks,song_name) # Pass actual integer song_id
 #         add_fingerprints_to_db(conn, song_id, song_hashes)
 # conn.close() # Disconnect when done adding
+
+
+
 from fingerprinting import *
 from pathlib import Path
 
 db_file = 'fingerprint_database.db'
 
-pathlist = Path('test_frags').glob('**/*.mp3')
+pathlist = Path('northern_cardinal_clips').glob('**/*.wav')
 for path in pathlist:
     # because path is object not string
     path_in_str = str(path)
@@ -27,7 +37,11 @@ for path in pathlist:
     peaks = find_peaks(spectrogram, sampling_rate)
     test_hashes = generate_fingerprints(peaks, 'test')
     match_name, score = match_sample_db(test_hashes, db_file)
-    print(f"Match result: Song='{match_name}', Score={score}\n")
+
+    if score > 30:
+        print(f"Match result: Song='{match_name}', Score={score}\n")
+    else:
+        print('No Match!\n')
 
 
 
